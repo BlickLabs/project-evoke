5edb29bf25702b4b67aa7068df22b055c676adbd
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-08-25 12:31:40 -0500

    "#9 added path"

--- src/js/menuScroll.js
+++ src/js/menuScroll.js
@@ -8,7 +8,7 @@ function doSomething(scroll_pos) {
     if (scroll_pos > 1) {
         siteNavbar.classList.add('nav-standar-bg');
         siteNavbar.classList.remove('nav-transparent-bg');
-    } else if (window.location.pathname === '/recursos.html') {
+    } else if (window.location.pathname === '/project-evoke/recursos.html') {
         if (scroll_pos >= 0) {
             siteNavbar.classList.add('nav-standar-bg');
         }
