2b6d2667e2c09608cb4f51ce2666b48acd823a29
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-08-24 21:48:20 -0500

    "#4 logo, hover change color depending on section"

--- src/js/menuScroll.js
+++ src/js/menuScroll.js
@@ -1,14 +1,14 @@
 var last_known_scroll_position = 0;
 var ticking = false;
 
+var siteNavbarLogo = document.querySelector('.site-navbar__desktop-logo-container img');
 var siteNavbar = document.querySelector('.site-navbar');
-var siteNavbarMenuLink = document.querySelector('.site-navbar__menu-link');
+var siteNavbarMenuLink = $('.site-navbar__menu-link');
 var siteNavbarActive = document.querySelector('.active');
 var siteLang = document.querySelector('.lang-div');
 var siteArrow = document.querySelector('.lang-arrow');
 
 // navbar-fixed-top
-
 function doSomething(scroll_pos) {
     if (scroll_pos > 1) {
         siteNavbar.classList.add('nav-standar-bg');
@@ -20,11 +20,16 @@ function doSomething(scroll_pos) {
             siteNavbarActive.classList.add('active-green');
             siteLang.classList.add('lang-div-green');
             siteArrow.classList.add('lang-arrow-green');
-            siteNavbarMenuLink.add('site-navbar__menu-link__green');
+            siteNavbarMenuLink.each(function (index, value) {
+                if (!$(value).hasClass('active-green')) {     
+                    $(value).addClass('site-navbar__menu-link__green')
+                }
+            });
+            siteNavbarLogo.setAttribute('src', 'img/evoke_verde_logo.png');
         }
     } else {
         siteNavbar.classList.add('nav-transparent-bg');
-        siteNavbar.classList.remove('nav-standar-bg')  
+        siteNavbar.classList.remove('nav-standar-bg');  
     }
 }
 
