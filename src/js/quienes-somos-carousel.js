e51d59ddcc4916279f4e33333385ae2071338d86
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-08-28 15:04:39 -0500

    "#8 added carousel for quienes-somos view"

---
+++ src/js/quienes-somos-carousel.js
@@ -0,0 +1,10 @@
+(function () {
+  $('.clientes-carousel__container').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    centerMode: true
+  });
+}());
