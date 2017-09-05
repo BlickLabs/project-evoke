dd258ffb7265d713e164c2a5f89ebd490a74166e
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-09-05 15:52:59 -0500

    "#1 added 's' to greatnes"

--- src/js/index-words.js
+++ src/js/index-words.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.index-banner-box1__title2', {
-  strings: ["genius", "potential", "greatnes"],
+  strings: ["genius", "potential", "greatness"],
   typeSpeed: 50,
   backSpeed: 30,
   backDelay: 700
