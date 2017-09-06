a20278ef719dca96eee53ddb2417873c71725c7f
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-09-05 23:41:56 -0500

    "#1 added loop to typed"

--- src/js/index-words.js
+++ src/js/index-words.js
@@ -2,5 +2,6 @@ var typed = new Typed('.index-banner-box1__title2', {
   strings: ["genius", "potential", "creativity", "innovation", "greatness"],
   typeSpeed: 50,
   backSpeed: 30,
-  backDelay: 700
+  backDelay: 700,
+  loop: true,
 });
