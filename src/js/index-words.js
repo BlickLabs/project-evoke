ccda41743bb868d2136bf0a7cf2ed4593883d9a4
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-09-05 19:32:53 -0500

    "#8 added a setTimeout function"

--- src/js/index-words.js
+++ src/js/index-words.js
@@ -1,6 +1,8 @@
-var typed = new Typed('.index-banner-box1__title2', {
-  strings: ["genius", "potential", "creativity", "innovation", "greatness"],
-  typeSpeed: 50,
-  backSpeed: 30,
-  backDelay: 700
-});
+setTimeout(function() {
+  var typed = new Typed('.index-banner-box1__title2', {
+    strings: ["genius", "potential", "creativity", "innovation", "greatness"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 700
+  });
+}, 1000);
